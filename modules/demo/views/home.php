c5f50413d03195ce97ed8f7e0a28d73b7f3248ea
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2009-04-26 22:50:43 +0000

    demo views added

---
+++ modules/demo/views/home.php
@@ -0,0 +1,11 @@
+Hi, this is Rapyd Demo Module
+
+Server Requirements:
+
+- php 5.1+, mysql 4.1+ (but you can add your own db-wrapper)
+
+Demo Requirements:
+
+- To run samples you need to config rapyd in /application/config.php ad setup database connection
+- Then dump this sql:
+
