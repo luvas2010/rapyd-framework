2cc80d3749281da039fc2167454eba9db090d3d7
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2009-08-03 22:54:04 +0000

    oop synstax for dataedit works, updated minor version number , new release will be available soon

--- modules/demo/controllers/edit.php
+++ modules/demo/controllers/edit.php
@@ -8,49 +8,23 @@ class edit_controller extends rpd {
 	{
 
 		//edit
-		$config = array	(
-			'label'  => 'Manage Article',
-			'source' => 'articles',
-			'back_url' => $this->url('filtered_grid'),
-			'fields' => array (
-					array (
-						'type'  => 'input',
-						'name'  => 'title',
-						'label' => 'Title',
-						'rule'  => 'trim|required',
-					),
+		$edit = new dataedit_library();
+		$edit->back_url = $this->url('filtered_grid');
 
-					array(
-						'type'   => 'radiogroup',
-						'name'   => 'public',
-						'label'  => 'Public',
-						'options'=> array("y"=>"Yes", "n"=>"No"),
-					),
-					array(
-						'type'   => 'dropdown',
-						'name'   => 'author_id',
-						'label'  => 'Author',
-						'option' => '',
-						'options'=> 'SELECT author_id, firstname FROM authors',
-						'rule'  => 'required',
-					),
-					array(
-						'type'  => 'date',
-						'name'  => 'datefield',
-						'label' => 'Date',
-						'attributes' => array('style'=>'width: 80px'),
-					),
-					array(
-						'type'  => 'editor',
-						'name'  => 'body',
-						'label' => 'Description',
-					),
-			),
-			'buttons'  => array('modify','save','undo','back'),
-		);
-		$edit = new dataedit_library($config);
+		$edit->set_label('Manage Article');
+		$edit->set_source('articles');
+
+		$edit->set_field('input','title','Title')->set_rule('trim','required');
+		$edit->set_field('radiogroup','public','Public')->set_options(array("y"=>"Yes", "n"=>"No"));
+		$edit->set_field('dropdown','author_id','Author')->set_options('SELECT author_id, firstname FROM authors')
+			 ->set_rule('required');
+
+		$edit->set_field('date','datefield','Date')->set_attributes(array('style'=>'width: 80px'));
+		$edit->set_field('editor','body','Description')->set_rule('required');
+
+		$edit->set_buttons('modify','save','undo','back');
+
 		$edit->build();
-
 
 		$data['head']			= $this->head();
 		$data['title'] 		= 'DataEdit';
