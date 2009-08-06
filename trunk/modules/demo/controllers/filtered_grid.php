d687c69924774755de3d3e97137857f652205f89
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2009-08-06 20:52:17 +0000

    merge felix to trunk - datafilter demo (new oop syntax)

--- trunk/modules/demo/controllers/filtered_grid.php
+++ trunk/modules/demo/controllers/filtered_grid.php
@@ -7,72 +7,40 @@ class filtered_grid_controller extends rpd {
 	function index()
 	{
 		//filter
-		$config = array	(
-			'label'   => 'Article Filter',
-			'fields'  => array(
-					array(
-						'type'  => 'input',
-						'name'  => 'title',
-						'label' => 'Title',
-						'style' => 'width:170px',
-					),
-					array(
-						'type'   => 'radiogroup',
-						'name'   => 'public',
-						'label'  => 'Public',
-						'options'=> array("y"=>"Yes", "n"=>"No"),
-					),
 
-			),
-			'buttons' => array('reset','search')
-		);
-		$filter = new datafilter_library($config);
+		$filter = new datafilter_library();
+		$filter->label = 'Article Filter';
 		$filter->db->select("articles.*, authors.*");
 		$filter->db->from("articles");
 		$filter->db->join("authors","authors.author_id=articles.author_id","LEFT");
+		$filter->field('input','title','Title')->attributes(array('style' => 'width:170px'));
+		$filter->field('radiogroup','public','Public')->options(array("y"=>"Yes", "n"=>"No"));
+		$filter->buttons('reset','search');
 		$filter->build();
 
-		//grid
-		$config = array	(
-			'label'     => 'Article List',
-			'source'    => $filter,
-			'per_page'  => 5,
-			'buttons'   => array('add'=>array('url'=>$this->url('edit?create=1'))),
-			'columns'   => array(
-				array(
-					'label'   => 'ID',
-					'pattern' => 'article_id',
-					'orderby' => true,
-					'url'     => $this->url('edit?show={article_id}'),
-					'img'     => 'detail.gif',
-				),
-				array(
-					'label'   => 'Title',
-					'pattern' => 'title',
-					'orderby' => true,
-				),
-				array(
-					'label'   => 'Body',
-					'pattern' => '<htmlspecialchars><substr>{body}|0|10</substr> </htmlspecialchars>',
-				),
-				array(
-					'label'   => 'Author',
-					'pattern' => '{firstname} {lastname}',
-				),
-			)
-		);
-		$grid = new datagrid_library($config);
+		$grid = new datagrid_library();
+		$grid->label = 'Article List';
+		$grid->per_page = 5;
+		$grid->source($filter);
+		$grid->column('article_id','ID',true)->url('edit?show={article_id}','detail.gif');
+		$grid->column('title','Title',true);
+		$grid->column('body','Body')->callback('escape');
+		$grid->column('{firstname} {lastname}','Author');
 		$grid->build();
 
-
-		$data['head']			= $this->head();
-		$data['title']		= 'DataGrid+DataFilter';
-		$data['content']	= $filter.'<br />'.$grid.'<br />';
-		$data['code']			= highlight_string(file_get_contents(__FILE__), TRUE);
+		$data['head']	= $this->head();
+		$data['title']	= 'DataGrid+DataFilter';
+		$data['content']= $filter.'<br />'.$grid.'<br />';
+		$data['code']	= highlight_string(file_get_contents(__FILE__), TRUE);
 
 		//output
 		echo $this->view('demo', $data);
 	}
 
+}
 
+
+function escape($row)
+{
+	return htmlspecialchars(substr($row['body'],0,10));
 }
