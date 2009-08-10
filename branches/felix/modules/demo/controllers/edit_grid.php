aaa9a4320dbd381740eb0c506cac37546ef99002
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2009-08-10 11:52:48 +0000

    debug, indentation fix etc..

--- branches/felix/modules/demo/controllers/edit_grid.php
+++ branches/felix/modules/demo/controllers/edit_grid.php
@@ -7,39 +7,25 @@ class edit_grid_controller extends rpd {
 	public function article()
 	{
 
-		//article dataedit configuration
-		$article_config = array	(
-			'label'  => 'Manage Article',
-			'source' => 'articles',
-			'fields' => array (
-					array (
-						'type'  => 'input',
-						'name'  => 'title',
-						'label' => 'Title',
-						'rule'  => 'trim|required',
-					),
-					array(
-						'type'  => 'editor',
-						'name'  => 'body',
-						'label' => 'Description',
-						'when'  => 'show|modify|update',
-					),
-					array(
-						'type'  => 'container',
-						'name'  => 'comments',
-					),
-					//etc...
-			),
-			'buttons'  => array('modify','save','undo'),
-		);
-		$article_edit = new dataedit_library($article_config);
+		//article dataedit
+
+		$article_edit = new dataedit_library();
+		$article_edit->label = 'Manage Articls';
+		$article_edit->source('articles');
+		$article_edit->field('input','title','Title')->rule('trim','required');
+		$article_edit->field('editor','body','Description')->rule('required');
+		$article_edit->field('container','comments','m');
+		$article_edit->buttons('modify','save','undo');
 		$article_edit->build();
 
-		if ($this->qs->value('create1|show1|modify1|update1|insert1|do_delete1'))
+		if ($this->qs->value('show'))
 		{
-			$article_edit->nest('comments',$this->comment());
-		} else {
-			$article_edit->nest('comments',$this->comments());
+			if ($this->qs->value('show1|modify1|update1|create1|insert1|do_delete1'))
+			{
+				$article_edit->nest('comments',$this->comment());
+			} else {
+				$article_edit->nest('comments',$this->comments());
+			}
 		}
 		$this->render($article_edit);
 	}
@@ -48,67 +34,41 @@ class edit_grid_controller extends rpd {
 	public function comment()
 	{
 
-		//comments dataedit configuration
-		$comment_config = array	(
-			'label'  => 'Manage Comment',
-			'source' => 'comments',
-			'back_url'=> $this->url('edit_grid/article?show='.$this->qs->value('show')),
-			'back_save' => true,
-			'back_undo' => true,
-			'fields' => array (
-					array(
-						'type'  => 'hidden',
-						'name'  => 'article_id',
-						'insert_value' => $this->qs->value('show'),
-					),
-					array(
-						'type'  => 'textarea',
-						'name'  => 'comment',
-						'label' => 'Comment',
-					),
-			),
-			'buttons'  => array('modify','save','undo','back'),
-		);
-		$comment_edit = new dataedit_library($comment_config);
+		//comments dataedit
+		$comment_edit = new dataedit_library();
+		$comment_edit->label = 'Manage Comment';
+		$comment_edit->back_url = $this->url('edit_grid/article?show='.$this->qs->value('show'));
+		$comment_edit->back_save = true;
+		$comment_edit->back_delete = true;
+		$comment_edit->back_cancel = true;
+		$comment_edit->back_cancel_save = true;
+		$comment_edit->source('comments');
+		$comment_edit->field('hidden','article_id','')->insert_value($this->qs->value('show'));
+		$comment_edit->field('textarea','comment','Comment');
+		$comment_edit->buttons('modify','save','undo','back');
 		$comment_edit->build();
-		return $comment_edit;
+
+		return $comment_edit->output;
 	}
 
 
 	public function comments()
 	{
 		//comments datagrid configuration
-		$comments_config = array (
-			'label'     => 'Comments List',
-			'source'    => 'comments',
-			'per_page'  => 20,
-			'buttons'   => array('add'),
-			'columns'   => array(
-				array(
-					'label'   => 'ID',
-					'pattern' => 'comment_id',
-					'orderby' => true,
-					'url'     => $this->url('edit_grid/article?show={article_id}&modify1={comment_id}'),
-					'img'     => 'detail.gif',
-				),
-				array(
-					'label'   => 'Comment',
-					'pattern' => 'comment',
-				),
-				array(
-					'label'   => 'delete',
-					'pattern' => 'delete',
-					'url'     => $this->url('edit_grid/article?show={article_id}&do_delete1={comment_id}'),
-					'img'     => '',
-				),
-			),
-		);
-		$comments_grid = new datagrid_library($comments_config);
+
+		$comments_grid = new datagrid_library();
+		$comments_grid->source('comments');
 		$comments_grid->db->where('article_id',$this->qs->value('create|show|modify|update'));
 		$comments_grid->db->orderby('comment_id','desc');
+		$comments_grid->column('comment_id', 'ID', true)
+					->url('edit_grid/article?show={article_id}&modify1={comment_id}','detail.gif');
+		$comments_grid->column('comment','Comment');
+		$comments_grid->column('delete', 'delete')
+					->url('edit_grid/article?show={article_id}&do_delete1={comment_id}');
+		$comments_grid->buttons('add');
 		$comments_grid->build();
 
-		return $comments_grid;
+		return $comments_grid->output;
 
 	}
 
