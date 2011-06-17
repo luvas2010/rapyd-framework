77d603c5d10bd829a05eef9b58155e20c0fa7476
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2011-06-17 18:30:41 +0000

    lang fix on some action

--- trunk/core/i18n/en_US.php
+++ trunk/core/i18n/en_US.php
@@ -4,41 +4,44 @@
 $lang = array
 (
   //dataedit
-	'de.inserted'	=> 'New record created.',
+	'de.inserted'=> 'New record created.',
 	'de.updated'=> 'The record has been updated.',
 	'de.deleted'=> 'The record has been deleted.',
 
-	'de.err_read'		=> 'Error reading record.',
+	'de.err_read'	=> 'Error reading record.',
 	'de.err_insert'	=> 'Error creating record.',
 	'de.err_update'	=> 'Error updating record.',
 	'de.err_delete'	=> 'Error deleting record.',
 	'de.err_unknown'=> 'Error, no record selected',
-
+	'de.err_dup_pk' => 'Error, duplicated primary key',
+	'de.err_no_model'=> 'Error, missed datamodel, use $edit->source("tablename")',
+	'de.err_no_backurl'=> 'Error, you must set "back_url" property',
+	
 	'de.confirm_delete'=> 'Do you want to eliminate the record?',
 	'de.inserted'=> 'Error, primary key not unique.',
 
-  //field text
+	//field text
 	'fld.delete'=> 'Erase',
 	'fld.browse'=> 'Select the file:',
 	'fld.modify'=> 'Modify',
 
-  //buttons
-	'btn.add'		=> 'Add',
+	//buttons
+	'btn.add'	=> 'Add',
 	'btn.reset'	=> 'Reset',
-	'btn.search'=> 'Search',
-	'btn.modify'=> 'Modify',
-	'btn.delete'=> 'Delete',
+	'btn.search'=>	'Search',
+	'btn.modify'=>	'Modify',
+	'btn.delete'=>	'Delete',
 
 	'btn.do_delete'	=> 'Delete',
-	'btn.save'			=> 'Save',
-	'btn.undo'			=> 'Undo',
-	'btn.back'			=> 'Back to the list',
+	'btn.save'	=> 'Save',
+	'btn.undo'	=> 'Undo',
+	'btn.back'	=> 'Back to the list',
 	'btn.back_edit'	=> 'Show',
 	'btn.back_error'=> 'Back',
 
 	// validations
 	'val.required'      => 'The %s field is required.',
-  'val.isset'         => 'The %s field is required.',
+	'val.isset'         => 'The %s field is required.',
 	'val.min_length'    => 'The %s field must be at least %d characters long.',
 	'val.max_length'    => 'The %s field must be %d characters or fewer.',
 	'val.exact_length'  => 'The %s field must be exactly %d characters.',
@@ -46,10 +49,10 @@ $lang = array
 	'val.valid_email'   => 'The %s field must contain a valid email address.',
 	'val.in_range'      => 'The %s field must be between %s and %s .',
 	'val.regex'         => 'The %s field does not match accepted input.',
-  'val.unique'		    => 'The %s field must be unique, there is another field with this value.',
-  'val.captcha'		    => 'The %s field does not match the captcha image, retry with the new one',
-  'val.approve'	      => 'You must approve %s .',
-  'val.valid_type'	  => 'The %s field can contain %s chars',
+	'val.unique'	    => 'The %s field must be unique, there is another field with this value.',
+	'val.captcha'	    => 'The %s field does not match the captcha image, retry with the new one',
+	'val.approve'	    => 'You must approve %s .',
+	'val.valid_type'	=> 'The %s field can contain %s chars',
 
 	// field types
 	'val.alpha'         => 'alphabetical',
@@ -65,9 +68,9 @@ $lang = array
 	'val.min_width'     => 'The %s file you uploaded was too small. The minimum allowed width is %spx.',
 	'val.min_height'    => 'The %s file you uploaded was too small. The minimum allowed height is %spx.',
 
-  // pagination
-  'pag.first'         => 'First',
-  'pag.last'          => 'Last',
+	// pagination
+	'pag.first'         => 'First',
+	'pag.last'          => 'Last',
 
 
 );
