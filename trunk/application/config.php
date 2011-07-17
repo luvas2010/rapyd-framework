a6441ca56e993c929d1eb4c70f86ab40e18c60fc
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2011-07-17 17:44:26 +0000

    new config var: output_compression, and some comment added

--- trunk/application/config.php
+++ trunk/application/config.php
@@ -6,11 +6,11 @@
  */
 
 $config['index_page'] = 'index.php'; //use 'index.php' if htaccess not allowed
-$config['basename'] = ""; //it correspont to .htaccess BASENAME
+$config['basename'] = ""; //it correspont to .htaccess RewriteBase  and should be "/foldername/" if you use the cms in subfolder of the docroot
 $config['url_method'] = "uri"; //alternative: "qs"  define if rapyd will use uri or query string for its semantic
-$config['default_controller'] = 'welcome';
-$config['default_method']     = 'index';
-
+$config['default_controller'] = 'welcome'; //default controller class to instance
+$config['default_method']     = 'index'; //default controller method to call
+$config['output_compression']  = 0; //output compression gzip level (if zlib is enabled) suggested level is 5 (0 means disabled)
 
 $config['include_paths'][]  = 'application';
 $config['include_paths'][]  = 'modules/demo';
