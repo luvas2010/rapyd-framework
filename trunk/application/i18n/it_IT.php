f677590136117695085513b7a5a3888f3d35542c
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2011-10-30 08:27:53 +0000

    cleanup of  lang application files

--- trunk/application/i18n/it_IT.php
+++ trunk/application/i18n/it_IT.php
@@ -3,18 +3,5 @@
 
 $lang = array
 (
-	'cms.payoff'      => 'Fabbrica Lampadari',
-	'cms.add'         => 'Aggiungi',
-	'cms.map'         => 'Mappa',
-	'cms.our'         => 'Il nostro',
-	'cms.catalog'     => 'Catalogo',
-    
-	'cms.furnish_an'  => 'Arreda un',
-	'cms.environment' => 'Ambiente',    
-	'cms.related_products'  => 'Prodotti Correlati',
-	'cms.ask_info'  => 'Chiedi Informazioni',
-    
-	'cms.search_into'   => 'Cerca nel catalogo',
-	'cms.search_results'=> 'Risultati della ricerca',
-	'cms.no_results'    => 'Nessun risultato',
+	//'prefix.lang_key'      => 'string',
 );
