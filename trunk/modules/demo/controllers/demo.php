4738d03bd3ac5c7cb0d2889bca2d8b93fcba4098
felice.ostuni <felice.ostuni@29079836-d8ad-11dd-a297-4fb579711a53>
2011-11-01 10:02:58 +0000

    demo page multilanguage, thnks to petr for cz, please all to send corrections for french

--- trunk/modules/demo/controllers/demo.php
+++ trunk/modules/demo/controllers/demo.php
@@ -9,7 +9,8 @@ class demo_controller extends rpd {
 
 		$data['head']		= $this->head();
 		$data['title'] 		= 'Repyd Demos';
-		$data['content']	= nl2br($this->view('home'));
+		
+		$data['content']	= nl2br($this->view('home_'.rpd::get_lang('locale')));
 		$data['code'] 		= '';
 
 		//output
